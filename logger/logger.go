c297115bee8a70ec5282f3fbeea14e6bf2f84566
dustyfresh <dustyfresh@localhost>
2024-02-10 13:13:34 -0600

    implementing LogToJSONLines

--- logger/logger.go
+++ logger/logger.go
@@ -1,16 +1,18 @@
 package logger
 
 import (
-	"context"
-	"io"
-	"time"
 	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"os"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson"
 
 	"helix-honeypot/model"
 )
@@ -18,7 +20,7 @@ import (
 type CustomLogger = model.CustomLogger
 
 type LocalCustomLogger struct {
-    *CustomLogger // Embed the CustomLogger type. It brings in all fields from the embedded type.
+	*CustomLogger // Embed the CustomLogger type. It brings in all fields from the embedded type.
 }
 
 // NewCustomLogger creates a new instance of CustomLogger.
@@ -38,24 +40,24 @@ func NewCustomLogger(cfg *model.Config) (*LocalCustomLogger, error) {
 
 		return &LocalCustomLogger{
 			CustomLogger: &CustomLogger{
-				Logger:              logger,
-				Collection:          collection,
+				Logger:               logger,
+				Collection:           collection,
 				EnableMongoDBLogging: true,
-				Cfg:                 cfg,
-				MachineID:           cfg.MachineID,
-				Location:			 cfg.RunMode.Location,
+				Cfg:                  cfg,
+				MachineID:            cfg.MachineID,
+				Location:             cfg.RunMode.Location,
 			},
 		}, nil
-		
+
 	}
 
 	return &LocalCustomLogger{
 		CustomLogger: &CustomLogger{
-			Logger:              logger,
+			Logger:               logger,
 			EnableMongoDBLogging: false,
-			Cfg:                 cfg,
-			MachineID:           cfg.MachineID,
-			Location:			 cfg.RunMode.Location,
+			Cfg:                  cfg,
+			MachineID:            cfg.MachineID,
+			Location:             cfg.RunMode.Location,
 		},
 	}, nil
 }
@@ -76,9 +78,9 @@ func (cl *LocalCustomLogger) Middleware(next echo.HandlerFunc) echo.HandlerFunc 
 			RemoteIP:  c.RealIP(),
 			Headers:   req.Header,
 			MachineID: cl.MachineID[:],
-			Location: cl.Location,
+			Location:  cl.Location,
 		}
-		
+
 		entryFields := httpLog.ToLogrusFields()
 
 		// Read the request body
@@ -109,6 +111,9 @@ func (cl *LocalCustomLogger) Middleware(next echo.HandlerFunc) echo.HandlerFunc 
 			if cl.Cfg.MongoDB.LogToMongoDB {
 				cl.LogToMongoDB(entryFields, startTime)
 			}
+
+			// Log to local JSON file
+			cl.LogToJSONLines(entryFields, startTime)
 		}
 
 		return nil
@@ -120,14 +125,14 @@ func (cl *LocalCustomLogger) LogToMongoDB(entryFields logrus.Fields, startTime t
 
 	// Create a document from the log entry
 	doc := bson.M{
-		"level":     entryFields["level"],
-		"message":   entryFields["message"],
-		"time":      time.Now(),
-		"fields":    entryFields,
-		"duration":  duration.String(),
+		"level":      entryFields["level"],
+		"message":    entryFields["message"],
+		"time":       time.Now(),
+		"fields":     entryFields,
+		"duration":   duration.String(),
 		"machine-id": cl.MachineID[:],
-		"remote-ip": entryFields["remote-ip"],
-		"location": cl.Location,
+		"remote-ip":  entryFields["remote-ip"],
+		"location":   cl.Location,
 	}
 
 	// Insert the document into MongoDB
@@ -135,5 +140,42 @@ func (cl *LocalCustomLogger) LogToMongoDB(entryFields logrus.Fields, startTime t
 	if err != nil {
 		// Handle the error if necessary
 		cl.Logger.WithError(err).Error("Failed to log to MongoDB")
+	}
+}
+
+func (cl *LocalCustomLogger) LogToJSONLines(entryFields logrus.Fields, startTime time.Time) {
+	duration := time.Since(startTime)
+
+	// Create a document from the log entry
+	doc := map[string]interface{}{
+		"level":      entryFields["level"],
+		"message":    entryFields["message"],
+		"time":       time.Now(),
+		"fields":     entryFields,
+		"duration":   duration.String(),
+		"machine-id": cl.MachineID[:],
+		"remote-ip":  entryFields["remote-ip"],
+		"location":   cl.Location,
+	}
+
+	// Serialize the document to JSON
+	jsonData, err := json.Marshal(doc)
+	if err != nil {
+		cl.Logger.WithError(err).Error("Failed to serialize log entry to JSON")
+		return
+	}
+
+	// Open file in append mode or create it if it doesn't exist
+	file, err := os.OpenFile("/helix.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		cl.Logger.WithError(err).Error("Failed to open log file")
+		return
+	}
+	defer file.Close()
+
+	// Write the JSON data to the file with a newline character
+	_, err = file.Write(append(jsonData, '\n'))
+	if err != nil {
+		cl.Logger.WithError(err).Error("Failed to write log entry to file")
 	}
 }
